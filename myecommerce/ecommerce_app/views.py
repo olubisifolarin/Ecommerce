cf43a78c20d6e862556a659321cfd5d6a98a3b49
olubisifolarin <hollubissy4real10@gmail.com>
2024-02-23 15:46:33 +0100

    created Search Component and Django Views

--- myecommerce/ecommerce_app/views.py
+++ myecommerce/ecommerce_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Search
 
 # Create your views here.
+def search(request):
+    query = request.GET.get("q")
+    search_result = Search.objects.filter(fielsa_to_search__icontains=query)
+    return render(request, "Search.jsx",{"search_result": search_result})
--- myecommerce/myecommerce/settings.py
+++ myecommerce/myecommerce/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'myecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL ='/media/'
+
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
