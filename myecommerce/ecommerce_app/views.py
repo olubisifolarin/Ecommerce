ed7b725b666e9c586c98881fb1165136c7f7a8db
olubisifolarin <hollubissy4real10@gmail.com>
2025-07-30 23:55:07 +0100

    made changes

--- myecommerce/ecommerce_app/views.py
+++ myecommerce/ecommerce_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Search
+from django.http import JsonResponse
+
 
 # Create your views here.
 def search(request):
     query = request.GET.get("q")
-    search_result = Search.objects.filter(fielsa_to_search__icontains=query)
-    return render(request, "Search.jsx",{"search_result": search_result})
+    search_result = Search.objects.filter(category__icontains=query)
+    return JsonResponse({'results': list(search_result.values())})
+   
