53933273bd153db385bff4fe3b97921452a02610
olubisifolarin <hollubissy4real10@gmail.com>
2025-07-31 00:21:46 +0100

    made changes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoMdSearch } from "react-icons/io";
 
 
 const Search = () => {
+    const[query, setQuery] = useState('');
+    const[results, setResults] = useState([]);
+
+    const handleSearch = async () => {
+        try {
+          const response = await fetch(`/api/search/?q=${query}`);
+          const data = await response.json();
+          setResults(data.results);
+        } catch (error) {
+          console.error('Error fetching search results:', error);
+        }
+      };
+
     return(
         <div className="searchBar mb-12">
-            <input type="text" placeholder="Search" className="inputField" />
-            <IoMdSearch className="searchIcon" />
+            <input type="text" value={query} placeholder="Search" 
+            className="inputField"
+            onChange={(e) => setQuery(e.target.value)}
+             />
+            <IoMdSearch className="searchIcon" onClick={handleSearch}/>
+           <ul>
+                {results.map((result) => (
+                    <li key={result.id}>{result.name}</li>
+                ))}
+           </ul>
         </div>
+        
     )
 }
 
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,8 +3,6 @@ import ShopCards from "../constant/ShopCards";
 import shirt from "../assets/shirt.png";
 import top from "../assets/top.png";
 import bag from "../assets/bag.webp";
-import bag2 from "../assets/bag2.webp";
-import crocs2 from "../assets/crocs2.png";
 import shoe from "../assets/shoe.png";
 import shoe1 from "../assets/shoe1.png";
 import pinkbag from "../assets/pinkbag.png";
@@ -12,8 +10,6 @@ import crocs from "../assets/crocs.png";
 import crocs3 from "../assets/crocs3.jpg";
 import gownbag from "../assets/gownbag.jpg";
 import shirt1 from "../assets/shirt1.jpg";
-import shirt2 from "../assets/shirt2.jpg";
-import boxer from "../assets/boxer.png";
 import boxer1 from "../assets/boxer1.png";
 import gown from "../assets/gown.png";
 
@@ -37,7 +33,7 @@ const Shop = () => {
                 />
 
                 <ShopCards
-                    image= {crocs2}
+                    image= {crocs}
                 />
 
                 <ShopCards
@@ -69,27 +65,9 @@ const Shop = () => {
                 />
 
                 <ShopCards
-                    image= {boxer}
-                />
-
-                <ShopCards
-                    image= {bag2}
-                />
-
-                <ShopCards
-                    image= {crocs}
-                />
-
-                <ShopCards
-                    image= {shirt2}
-                />
-
-                <ShopCards
                     image= {shoe1}
                 />
-
-                
-
+        
             </div>
         </div>
 
