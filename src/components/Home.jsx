6a8367d02daf06056f884d5870027228923367a6
olubisifolarin <hollubissy4real10@gmail.com>
2025-07-31 23:17:41 +0100

    made changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,29 +7,38 @@ import Services from "./Services";
 import Shop from "./Shop";
 
 const Home = () => {
-    return(
-        <>
-        <div className="home text-white flex">
-            <div className="w-1/2 elegantWears">
-                <h1 className="welcome">Welcome to Elegant Wears, the place where style and comfort collide to make fashion a statement!</h1>
-                    <div className="shop-btn pt-2 text-center">
-                        <button>Shop Now</button>
-                    </div>  
-            </div>
-            <div className="w-1/2 home-img flex relative">
-                <img src={imgbg} alt="home-img" className="gown"/>
-                
-                <div className="block boxerPink absolute">
-                    <img src={boxer} className="boxer mt-10" alt="home-img" />
-                    <img src={pinkbag} className="pinkBag" alt="home-img" />
-                </div>
-                <img src={crocs} className="crocs relative" alt="home-img" />
-            </div>   
+  return (
+    <>
+      <div className="flex flex-wrap items-center justify-between px-4 py-10 md:px-12">
+        {/* Text Section */}
+        <div className="w-full md:w-1/2 mb-8 md:mb-0">
+          <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-white">
+            Welcome to Elegant Wears, the place where style and comfort collide to make fashion a statement!
+          </h1>
+          <div className="pt-6">
+            <button className="bg-yellow-700 hover:bg-yellow-700 text-white font-semibold py-2 px-6 rounded-md border border-white">
+              Shop Now
+            </button>
+          </div>
         </div>
-        < Services />
-        <Shop />
-     </>
-    )
-}
+
+        {/* Image Section */}
+        <div className="w-full md:w-1/2 relative flex justify-center items-center">
+          <img src={imgbg} alt="home background" className="w-full max-w-md object-contain" />
+
+          <div className="absolute top-0 left-0 flex flex-col items-start gap-4 p-4">
+            <img src={boxer} alt="boxer" className="w-20 md:w-28 mt-10" />
+            <img src={pinkbag} alt="pink bag" className="w-16 md:w-24" />
+          </div>
+
+          <img src={crocs} alt="crocs" className="absolute bottom-0 right-4 w-24 md:w-32" />
+        </div>
+      </div>
+
+      <Services />
+      <Shop />
+    </>
+  );
+};
 
 export default Home;
