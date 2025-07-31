6985f34a34c034854363df6b4d3b72396880c1ec
olubisifolarin <hollubissy4real10@gmail.com>
2025-07-31 01:26:27 +0100

    made changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,17 +16,17 @@ const Navbar = () => {
     }
     return(
         <>
-        <nav className="flex">
+        <nav className="flex items-center justify-between p-4 relative bg-white">
             <Link to="/" className="nav-logo" onClick={closeMenu}>
                 <img src={elegant} alt="Elegant Logo" className="logo"/>
             </Link>
             <Search />
 
-            <div onClick={handleClick} className="bg-white rounded-full nav-icon">
+            <div onClick={handleClick} className="text-4xl cursor-pointer mr-10 mb-6 bg-white p-2 rounded-full z-50">
                 {open ? <FiX /> : <RiMenu4Line />}
             </div>
-
-            <ul className={`nav-links ${open ? 'active' : ''}`}>
+            <ul className={`flex flex-col absolute top-28 left-0 w-full bg-white transition-all duration-500 ease-in-out z-40 ${open ? "left-0" : "-left-full"}`}></ul>
+            {/* <ul className={`nav-links ${open ? 'active' : ''}`}> */}
         
                 <li className="nav-item">
                     <Link to="/" className="nav-link" onClick={closeMenu}>
